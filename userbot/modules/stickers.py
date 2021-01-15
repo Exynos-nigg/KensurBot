55c970a1115241468df96fa38473d7e8f339286b
Exynos-nigg <vsht700@gmail.com>
2021-01-15 22:22:28 +0400

    stickers: adapt to kang into my existing pack

--- userbot/modules/stickers.py
+++ userbot/modules/stickers.py
@@ -98,8 +98,8 @@ async def kang(args):
                 # pack
                 emoji = splat[1]
 
-        packname = f"a{user.id}_by_{user.username}_{pack}"
-        packnick = f"@{user.username}'s kang pack Vol.{pack}"
+        packname = f"a{user.id}_by_userge_{pack}"
+        packnick = f"@userge's kang pack Vol.{pack}"
         cmd = "/newpack"
         file = io.BytesIO()
 
@@ -127,8 +127,8 @@ async def kang(args):
                 x = await conv.get_response()
                 while "120" in x.text:
                     pack += 1
-                    packname = f"a{user.id}_by_{user.username}_{pack}"
-                    packnick = f"@{user.username}'s kang pack Vol.{pack}"
+                    packname = f"a{user.id}_by_userge_{pack}"
+                    packnick = f"@userge's kang pack Vol.{pack}"
                     await args.edit("**Switching to Pack " + str(pack) +
                                     " due to insufficient space...**")
                     await conv.send_message(packname)
